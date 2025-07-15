d18102179d6ce5e3fa36ca9eae41888007812125
Nichu055 <ulasiebuka2@gmail.com>
2025-07-16 00:39:08 +0100

    route login page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
