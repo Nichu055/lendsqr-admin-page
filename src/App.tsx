ca2ebfb1d4bca5b655ff6dd9493a9178a8312e10
Nichu055 <ulasiebuka2@gmail.com>
2025-07-16 18:09:53 +0100

    update user router from /user to /users

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
       element: <DashboardOutlet />,
       children: [
         {
-          path: "user",
+          path: "users",
           element: <User />
         },
       ],
