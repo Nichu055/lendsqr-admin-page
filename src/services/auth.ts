2cd0a9f1447fb374407a47c7edbe0a6e00939b58
Nichu055 <ulasiebuka2@gmail.com>
2025-07-17 04:27:28 +0100

    add demouser

---
+++ src/services/auth.ts
@@ -0,0 +1,4 @@
+export const DEMO_USERS = [
+  { id: 1, email: 'admin@lendsqr.com', password: 'admin123', name: 'Admin User' }
+//   { id: 2, email: 'user@lendsqr.com', password: 'user123', name: 'Regular User' }
+];
